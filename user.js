b736df724e0bcefb11b5b61a22f2ce6b9c917202
Developer <dev@localhost>
2014-01-26 23:34:05 +0400

    ---

--- user.js
+++ user.js
@@ -6,18 +6,19 @@ $DOC.sections['fixed-top-bar'] =
 ***\
 [/navbar]';
 
+/*
 $DOC.sections['header-panel'] =
 '# Markdown webdocs\n\
 Система подготовки веб-справки и документации  \n\
 Редактор документов с Markdown разметкой';
-
+*/
 
 if ($OPT.editable) {
     $DOC.sections['footer-panel'] =
 '[footer-layout scheme=line]\
-* © 2013 [aplib on GitHub](https://github.com/aplib/markdown-site-template) Artistic license 2.0\n\
 * [repo on GitHub](https://github.com/facdev/docs)\n\
 ***\n\
+* © 2013 [aplib on GitHub](https://github.com/aplib/markdown-site-template) Artistic license 2.0\n\
 * [Редактировать эту страницу](?edit)\
 [/footer-layout]';
 
